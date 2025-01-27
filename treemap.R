067a6dda26fe80f2890297a924f713d5b8324919
fdzul <felipe.dzul.m@gmail.com>
2025-01-27 10:27:02 -0600

    This commit update the dash

--- treemap.R
+++ treemap.R
@@ -8,3 +8,38 @@ x <- data.table::fread(path_sinave,
 densnv::mp_treemap(country = TRUE,
                    year = 2025,
                    snv_dataset = x)
+
+
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = "14",
+                   snv_dataset = x)
+
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = "30",
+                   snv_dataset = x)
+
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = "25",
+                   snv_dataset = x)
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = "28",
+                   snv_dataset = x)
+
+
+path_sinave <- "/Users/felipedzul/Library/CloudStorage/OneDrive-Personal/datasets/DGE/denv/2025/DENGUE2_.txt"
+x <- data.table::fread(path_sinave,
+                       #select = vect_cols2,
+                       encoding = "Latin-1",
+                       quote="",
+                       fill=TRUE)
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = "27",
+                   snv_dataset = x)
+
+
+
