56671dbe976f0a22ceb324e7a6612f0a6f53d596
fdzul <felipe.dzul.m@gmail.com>
2025-01-27 10:13:34 -0600

    This commit update the file

---
+++ treemap.R
@@ -0,0 +1,10 @@
+path_sinave <- "/Users/felipedzul/Library/CloudStorage/OneDrive-Personal/datasets/DGE/denv/2025/DENGUE2_.txt"
+x <- data.table::fread(path_sinave,
+                       #select = vect_cols2,
+                       encoding = "Latin-1",
+                       quote="",
+                       fill=TRUE)
+
+densnv::mp_treemap(country = TRUE,
+                   year = 2025,
+                   snv_dataset = x)
