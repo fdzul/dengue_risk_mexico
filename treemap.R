6dc9276895560fdf3d82dc51ed596d9b99559c76
fdzul <felipe.dzul.m@gmail.com>
2025-02-05 12:41:26 -0600

    This commit update the dash

--- treemap.R
+++ treemap.R
@@ -35,10 +35,16 @@ x <- data.table::fread(path_sinave,
                        #select = vect_cols2,
                        encoding = "Latin-1",
                        quote="",
-                       fill=TRUE)
+                       fill=TRUE) 
+
 densnv::mp_treemap(country = FALSE,
                    year = 2025,
-                   cve_edo = "27",
+                   cve_edo = 2,
+                   snv_dataset = x)
+
+densnv::mp_treemap(country = FALSE,
+                   year = 2025,
+                   cve_edo = 7,
                    snv_dataset = x)
 
 
